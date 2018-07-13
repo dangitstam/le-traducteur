d6ac242bb6c5714844512dddf9a266e42e67ca43
dangitstam <dangt7@uw.edu>
2018-07-12 17:37:28 -0700

    smoke-tested new dataset reader for pretokenized utterances

--- library/dataset_readers/europarl_english_french.py
+++ library/dataset_readers/europarl_english_french.py
@@ -9,7 +9,7 @@ from allennlp.data.fields import TextField
 from allennlp.data.instance import Instance
 from allennlp.data.token_indexers import SingleIdTokenIndexer, TokenIndexer
 from allennlp.data.tokenizers import Tokenizer, WordTokenizer
-from allennlp.data.tokenizers.word_splitter import SpacyWordSplitter
+from allennlp.data.tokenizers.word_splitter import JustSpacesWordSplitter, SpacyWordSplitter
 from overrides import overrides
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
@@ -100,3 +100,72 @@ class EuroparlEnglishFrenchReader(DatasetReader):
         params.assert_empty(cls.__name__)
         return cls(lazy=lazy, en_tokenizer=en_tokenizer, en_token_indexers=en_token_indexers,
                               fr_tokenizer=fr_tokenizer, fr_token_indexers=fr_token_indexers)
+
+@DatasetReader.register("europarl_parallel_english_french_pretokenized")
+class EuroparlEnglishFrenchReaderPretokenized(DatasetReader):
+    """
+    Identical to ``EuroparlEnglishFrenchReader`` but assumes its input is already tokenized.
+
+    Each ``read`` yields a data instance of
+        en: ``TextField``
+        fr: ``TextField``
+
+    Parameters
+    ----------
+    lazy : ``bool`` (optional, default=False)
+        Passed to ``DatasetReader``.  If this is ``True``, training will start sooner, but will
+        take longer per batch.  This also allows training with datasets that are too large to fit
+        in memory.
+    en_token_indexers : ``Dict[str, TokenIndexer]``, optional
+        Indexers used to define English token representations. Defaults to ``{"tokens":
+        SingleIdTokenIndexer(namespace="en", lowercase_tokens=True)}``.
+    fr_token_indexers : ``Dict[str, TokenIndexer]``, optional
+        Indexers used to define French token representations. Defaults to ``{"tokens":
+        SingleIdTokenIndexer(namespace="fr", lowercase_tokens=True)}``.
+    """
+    def __init__(self,
+                 lazy: bool = False,
+                 en_token_indexers: Dict[str, TokenIndexer] = None,
+                 fr_token_indexers: Dict[str, TokenIndexer] = None) -> None:
+        super().__init__(lazy)
+        self._en_tokenizer = WordTokenizer(word_splitter=JustSpacesWordSplitter())
+        self._fr_tokenizer = WordTokenizer(word_splitter=JustSpacesWordSplitter())
+        self._en_token_indexers = en_token_indexers or {
+            "tokens": SingleIdTokenIndexer(namespace="en", lowercase_tokens=True)
+        }
+        self._fr_token_indexers = fr_token_indexers or {
+            "tokens": SingleIdTokenIndexer(namespace="fr", lowercase_tokens=True)
+        }
+
+    @overrides
+    def _read(self, file_path):
+        with open(cached_path(file_path), 'r') as data_file:
+            logger.info("Reading instances from lines in file: %s", file_path)
+            for line in data_file:
+                line = line.strip("\n")
+                if not line:
+                    continue
+                parallel_utterance = json.loads(line)
+                en_utterance = parallel_utterance['en']
+                fr_utterance = parallel_utterance['fr']
+                yield self.text_to_instance(en_utterance, fr_utterance)
+
+    @overrides
+    def text_to_instance(self, en_utterance: str, fr_utterance: str) -> Instance:  # type: ignore
+        en_utterance_tokenized = self._en_tokenizer.tokenize(en_utterance)
+        fr_utterance_tokenized = self._fr_tokenizer.tokenize(fr_utterance)
+        fields = {
+            'en': TextField(en_utterance_tokenized, self._en_token_indexers),
+            'fr': TextField(fr_utterance_tokenized, self._fr_token_indexers)
+        }
+        return Instance(fields)
+
+    @classmethod
+    def from_params(cls, params: Params) -> 'EuroparlDatasetReader':
+        lazy = params.pop('lazy', False)
+        en_token_indexers = TokenIndexer.dict_from_params(params.pop('en_token_indexers', {}))
+        fr_token_indexers = TokenIndexer.dict_from_params(params.pop('fr_token_indexers', {}))
+        params.assert_empty(cls.__name__)
+        return cls(lazy=lazy, 
+                   en_token_indexers=en_token_indexers,
+                   fr_token_indexers=fr_token_indexers)
--- tests/dataset_readers/europarl_english_french_test.py
+++ tests/dataset_readers/europarl_english_french_test.py
@@ -1,49 +1,69 @@
-
-
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.common.util import ensure_list
 
-from library.dataset_readers.europarl_english_french import \
-    EuroparlEnglishFrenchReader
+from library.dataset_readers.europarl_english_french import (EuroparlEnglishFrenchReader,
+                                                             EuroparlEnglishFrenchReaderPretokenized)
 
 
 class TestEuroparlEnglishFrenchReader(AllenNlpTestCase):
+    # Hard-coded excerpts from the corpus for smoke testing.
+    INSTANCE_0 = {
+        "id": 0,
+        "en": ["Resumption", "of", "the", "session"],
+        "fr": ["Reprise", "de", "la", "session"]
+    }
+    INSTANCE_1 = {
+        "id": 1,
+        "en": ["I", "declare", "resumed", "the", "session", "of", "the", "European",
+                "Parliament", "adjourned", "on", "Friday", "17", "December", "1999",
+                ",", "and", "I", "would", "like", "once", "again", "to", "wish",
+                "you", "a", "happy", "new", "year", "in", "the", "hope", "that",
+                "you", "enjoyed", "a", "pleasant", "festive", "period", "."],
+        "fr": ["Je", "déclare", "reprise", "la", "session", "du", "Parlement",
+                "européen", "qui", "avait", "été", "interrompue", "le", "vendredi",
+                "17", "décembre", "dernier", "et", "je", "vous", "renouvelle", "tous",
+                "mes", "vux", "en", "espérant", "que", "vous", "avez", "passé", "de",
+                "bonnes", "vacances", "." ]
+    }
+    INSTANCE_7 = {
+        "id": 7, 
+        "en": ["Madam", "President", ",", "on", "a", "point", "of", "order", "."],
+        "fr": ["Madame", "la", "Présidente", ",", "c'", "est", "une", "motion", "de",
+                "procédure", "."]
+    }
+
+    DATASET_PATH = 'tests/fixtures/smoke_europarl_en_fr.jsonl'
+    PRETOKENIZED_DATASET_PATH = 'tests/fixtures/smoke_europarl_en_fr_tokenized.jsonl'
+
     def test_read_from_file(self):
         reader = EuroparlEnglishFrenchReader()
-        dataset = reader.read('tests/fixtures/smoke_europarl_en_fr.jsonl')
+        dataset = reader.read(TestEuroparlEnglishFrenchReader.DATASET_PATH)
         instances = ensure_list(dataset)
-        instance0 = {
-            "id": 0,
-            "en": ["Resumption", "of", "the", "session"],
-            "fr": ["Reprise", "de", "la", "session"]
-        }
-        instance1 = {
-            "id": 1,
-            "en": ["I", "declare", "resumed", "the", "session", "of", "the", "European",
-                   "Parliament", "adjourned", "on", "Friday", "17", "December", "1999",
-                   ",", "and", "I", "would", "like", "once", "again", "to", "wish",
-                   "you", "a", "happy", "new", "year", "in", "the", "hope", "that",
-                   "you", "enjoyed", "a", "pleasant", "festive", "period", "."],
-            "fr": ["Je", "déclare", "reprise", "la", "session", "du", "Parlement",
-                   "européen", "qui", "avait", "été", "interrompue", "le", "vendredi",
-                   "17", "décembre", "dernier", "et", "je", "vous", "renouvelle", "tous",
-                   "mes", "vux", "en", "espérant", "que", "vous", "avez", "passé", "de",
-                   "bonnes", "vacances", "." ]
-        }
-        instance7 = {
-            "id": 7, 
-            "en": ["Madam", "President", ",", "on", "a", "point", "of", "order", "."],
-            "fr": ["Madame", "la", "Présidente", ",", "c'", "est", "une", "motion", "de",
-                   "procédure", "."]
-        }
 
         assert len(instances) == 10
         fields = instances[0].fields
-        assert [t.text for t in fields["en"].tokens] == instance0["en"]
-        assert [t.text for t in fields["fr"].tokens] == instance0["fr"]
+        assert [t.text for t in fields["en"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_0["en"]
+        assert [t.text for t in fields["fr"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_0["fr"]
         fields = instances[1].fields
-        assert [t.text for t in fields["en"].tokens] == instance1["en"]
-        assert [t.text for t in fields["fr"].tokens] == instance1["fr"]
+        assert [t.text for t in fields["en"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_1["en"]
+        assert [t.text for t in fields["fr"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_1["fr"]
         fields = instances[7].fields
-        assert [t.text for t in fields["en"].tokens] == instance7["en"]
-        assert [t.text for t in fields["fr"].tokens] == instance7["fr"]
+        assert [t.text for t in fields["en"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_7["en"]
+        assert [t.text for t in fields["fr"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_7["fr"]
+
+
+    def test_read_from_file_pretokenized(self):
+        reader = EuroparlEnglishFrenchReaderPretokenized()
+        dataset = reader.read(TestEuroparlEnglishFrenchReader.PRETOKENIZED_DATASET_PATH)
+        instances = ensure_list(dataset)
+
+        assert len(instances) == 10
+        fields = instances[0].fields
+        assert [t.text for t in fields["en"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_0["en"]
+        assert [t.text for t in fields["fr"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_0["fr"]
+        fields = instances[1].fields
+        assert [t.text for t in fields["en"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_1["en"]
+        assert [t.text for t in fields["fr"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_1["fr"]
+        fields = instances[7].fields
+        assert [t.text for t in fields["en"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_7["en"]
+        assert [t.text for t in fields["fr"].tokens] == TestEuroparlEnglishFrenchReader.INSTANCE_7["fr"]
